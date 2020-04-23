2417e796850df4567715b5b76e8a4dcfff3dd49c
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-23 10:35:01 -0700

    add send and receive logic

--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgjms.sender;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -14,16 +17,31 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 5000)
     public void sendMessage() {
-        System.out.println("Sending a message");
-
+        //System.out.println("Sending a message");
         HelloWorldMessage message =
             HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello World!").build();
-
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+    }
 
-        System.out.println("Message Sent");
+    @Scheduled(fixedRate = 5000)
+    public void sendReceiveMessage() throws JMSException {
+        //System.out.println("Sending a message");
+        HelloWorldMessage message =
+            HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello Send/Rec World!").build();
+        Message recMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, session -> {
+            Message helloMessage;
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+            } catch (Exception e) {
+                throw new JMSException(e.getMessage());
+            }
+            return helloMessage;
+        });
+        System.out.println(recMessage.getBody(String.class));
     }
 }
